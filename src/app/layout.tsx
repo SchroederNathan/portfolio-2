3ad55c7c75c4aee3bc7d2d41d6b247d6f3279943
“Nathan <schroedernathan2011@icloud.com>
2025-11-20 19:18:18 -0500

    Added transition effect when toggling theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange
         >
           <ExposureFontLoader />
 
