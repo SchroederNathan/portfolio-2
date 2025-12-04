a6e4253b8a76b5df5a55584b441e11ebac75c9e8
“Nathan <schroedernathan2011@icloud.com>
2025-12-03 23:10:45 -0500

    Fixed background color for experience card hover state

--- src/components/experience/card.tsx
+++ src/components/experience/card.tsx
@@ -30,7 +30,7 @@ const ExperienceCard = ({ experience }: { experience: Experience }) => {
   const content = (
     <div className="group flex flex-row flex-basis relative pb-1 transition-all md:grid-cols-8 md:gap-8 lg:gap-4 lg:hover:opacity-100! lg:group-hover/list:opacity-50">
       {/* Hover background effect */}
-      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-foreground/5 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
 
       {/* Date header - hidden on small screens */}
       <header className="basis-1/4 max-md:hidden text-muted z-10">
